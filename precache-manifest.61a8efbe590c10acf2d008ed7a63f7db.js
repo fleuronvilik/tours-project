71b07c899375454c77c00c9b6bb6960909b75388
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-09 15:54:04 +0100

    Updates

---
+++ precache-manifest.61a8efbe590c10acf2d008ed7a63f7db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a060fadbbfd062e41e223efda94546b7",
+    "url": "/tours-project/index.html"
+  },
+  {
+    "revision": "fc39c94ba4765235e89b",
+    "url": "/tours-project/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/tours-project/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours-project/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc39c94ba4765235e89b",
+    "url": "/tours-project/static/js/main.871890ef.chunk.js"
+  },
+  {
+    "revision": "dbdfe489273ec43c1716",
+    "url": "/tours-project/static/js/runtime-main.0f695b42.js"
+  }
+]);
