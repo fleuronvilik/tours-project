29165810687e027b92601d86f086dcdea7e63c2f
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-27 22:21:29 +0100

    Add Loading and Empty states

--- src/App.js
+++ src/App.js
@@ -5,31 +5,59 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-/*   const getTours = async () => {
-    const response = await fetch(url)
-    const tours = await response.json()
-    return tours;
-  } */
-
   const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [empty, setEmpty] = useState(false);
 
   const updateInterests = function(id) {
     setTours(prevState => {
+      if (prevState.length === 1) {
+        setEmpty(true);
+      } 
       return prevState.filter(tour => tour.id !== id)
     })
   }
 
+  const refresh = () => {
+    setLoading(true);
+    setEmpty(false);
+  }
+
   useEffect(() => {
-    //getTours(); id, image, info, name price
     fetch(url)
       .then(response => response.json())
-      .then(arr => setTours(arr))
+      .then(arr => {
+        setTours(arr)
+        setLoading(false);
+      })
       .catch(err => console.error(err));
-  }, [])
+  }, [loading])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if (empty) {
+    return (
+      <main>
+        <header className="title">
+          <h2>No Tours Left</h2>
+          <button className="btn" onClick={refresh}>Refresh</button>
+        </header>
+      </main>
+    )
+  }
 
   return (
     <main>
-      <h2 className="title">Our Tours</h2>
+      <header className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </header>
       <Tours tours={tours} update={updateInterests}/>
     </main>
   )
